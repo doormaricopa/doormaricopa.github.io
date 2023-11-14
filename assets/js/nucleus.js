13d4143063eba7296d54e88c094effe8b0cbbc4a
teeling <kevin@teeling.org>
2023-11-13 23:24:20 -0700

    nucleus again

---
+++ assets/js/nucleus.js
@@ -0,0 +1,11 @@
+window.nucleusLauncherSettings = {
+    flowId: '4e7ab41bdcc1b8abd5cd',
+};
+(function (l, a, u, n, c, h) {
+    u = a.createElement('script');
+    u.type = 'text/javascript';
+    u.async = true;
+    u.src = 'https://launcher.nucleus.church/boot.latest.js';
+    n = a.getElementsByTagName('script')[0];
+    n.parentNode.insertBefore(u, n);
+})(window, document);
